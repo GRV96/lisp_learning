0ed07ffde951b676e16d16ad7bb41694a1b6fe44
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-10-18 21:47:54 -0400

    A more complex s-expression

--- myprog.lisp
+++ myprog.lisp
@@ -1,2 +1,10 @@
 ; https://www.tutorialspoint.com/lisp/lisp_program_structure.htm
+; 7 + 9 + 11 = 27
 (write (+ 7 9 11))
+
+; TERPRI (terminate printing) writes a new line.
+(terpri)
+
+; Convert 60°F to °C
+; (60 * 9 / 5 ) + 32 = 140
+(write (+ (* (/ 9 5) 60) 32))
