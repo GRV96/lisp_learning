14e412b353e8ccc9732e8c719e01c2adbfe93e89
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-10-18 22:36:14 -0400

    String formatting

--- myprog.lisp
+++ myprog.lisp
@@ -1,12 +1,10 @@
 ; https://www.tutorialspoint.com/lisp/lisp_program_structure.htm
 ; 7 + 9 + 11 = 27
 (setq sum (+ 7 9 11))
-(write sum)
-
-; TERPRI (terminate printing) writes a new line.
-(terpri)
+; ~%: new line
+(format t "~a~%" sum)
 
 ; Convert 60°F to °C
 ; (60 * 9 / 5 ) + 32 = 140
 (setq tempc (+ (* (/ 9 5) 60) 32))
-(write tempc)
+(format t "~a" tempc)
