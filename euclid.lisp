39b393d7256fee8edc5f40a024c418c05054f89f
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-10-19 21:07:00 -0400

    A function performs the Euclidean algorithm.

---
+++ euclid.lisp
@@ -0,0 +1,21 @@
+(defun euclid(a b)
+  (if (or (< a 0) (< b 0) (< a b))
+    (return nil))
+
+  (loop
+    (setq r (rem a b))
+
+    (if (zerop r)
+      (return b))
+
+    (setq a b)
+    (setq b r)
+  )
+)
+
+(defun print_gcd(a b)
+  (setq pgcd (euclid a b))
+  (format t "gcd(~a, ~a) = ~a" a b pgcd)
+)
+
+(print_gcd 360 240)
