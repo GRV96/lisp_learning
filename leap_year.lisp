7ac6798adc7ecead3e9f0ca7225af9ef942fee51
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2022-10-20 11:40:01 -0400

    Function leap-year

---
+++ leap_year.lisp
@@ -0,0 +1,13 @@
+(defun leap-year(year)
+  (setq r4 (rem year 4))
+  (setq r100 (rem year 100))
+  (setq r400 (rem year 400))
+  ; Year 0 does not exist.
+  (and (/= year 0) (or (and (= r4 0) (/= r100 0)) (= r400 0)))
+)
+
+(setq y (parse-integer (car *args*)))
+
+(setq leap (leap-year y))
+
+(format t "~a is a leap year: ~a." y leap)
