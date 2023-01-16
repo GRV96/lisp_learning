d8a7d972dd8c9b84aa579e0b5881a0e46dd7e44d
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-01-15 22:44:39 -0500

    Fibonacci numbers

---
+++ fibonacci.lisp
@@ -0,0 +1,26 @@
+(defun fibonacci(n)
+  (if (< n 0)
+    (return-from fibonacci nil))
+
+  (if (= n 0)
+    (return-from fibonacci 0))
+
+  (if (= n 1)
+    (return-from fibonacci 1))
+
+  (setq f0 0)
+  (setq f1 1)
+  (setq stop (- n 1))
+
+  (loop for i from 1 to stop do
+    (setq sum (+ f1 f0))
+    (setq f0 f1)
+    (setq f1 sum))
+
+  (return-from fibonacci f1)
+)
+
+(setq n (parse-integer (car *args*)))
+(setq fibo (fibonacci n))
+
+(format t "fibonacci(~a) = ~a" n fibo)
